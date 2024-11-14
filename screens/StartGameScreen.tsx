9dcc1a9bc9682136c0cefd2f93bf2150b2649523
TRESS\Lbarajas <luis.barajas@tress.com.mx>
2024-11-13 17:31:07 -0800

    initial commit

---
+++ screens/StartGameScreen.tsx
@@ -0,0 +1,11 @@
+import { TextInput, Button, View } from "react-native";
+
+function StartScreen() {
+  return (
+    <View>
+      <TextInput />
+    </View>
+  );
+}
+
+export default StartScreen;
